24265aa42ca006536221103ec56a8d7e1c1511a1
José Henrique <henriquedutra026@gmail.com>
2021-10-23 16:54:59 -0300

    atualização

--- lib/app/module/main/main_binding.dart
+++ lib/app/module/main/main_binding.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-import 'package:auresgate/app/module/main/main_controller.dart';
 import 'package:auresgate/app/module/request_rescue/request_rescue_controller.dart';
 import 'package:get/get.dart';
 
